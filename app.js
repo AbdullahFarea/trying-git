df5cf43daba84b89c3d3de5a6798d75e86121288
Abdullah Farea <87715865+AbdullahFarea@users.noreply.github.com>
2021-08-01 23:22:46 +0300

    added a new feature

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express   = require('express')
 const app       = express()
+const session   = require('express-session')
 require('dotenv').config()
 const hostname  = process.env.HOSTNAME || "localhost"
 const port      = process.env.PORT || 3000
 
-
+app.use(session({
+    secret: process.env.SECRET ,
+    saveUninitialized: false,
+    resave: false
+}))
 
 app.get('/', (req, res) => {
-    res.send('hello world!')
+    res.send('Hello, ' + req.session.userName)
 })
 
+app.get('/name/:name', (req, res) => {
+    req.session.userName = req.params.name
+    res.send(req.session.userName)
+})
 
 
 
